0f10ec604fb45dd2e040a76180ba9bce97141942
Ondross <andrew.heine@students.olin.edu>
2011-03-07 21:43:02 -0500

    first commit again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spyopolis::Application.config.secret_token = 'f76bccbd2d70a0cf4c6aaab343db12802fe063b27f59f9d95acb23802958a62901fc17042a45bad8b559977f58c8418d5cf540c9ae300317dc44fac35d3e3360'
